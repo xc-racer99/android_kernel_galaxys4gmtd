6c58efc0f82801b1b750ebec693f1dd7baffd096
Jeff Kletsky <aokp@allycomm.com>
2012-08-15 19:14:04 -0700

    Tune smartassv2 governor *  600 MHz awake target * 1000 MHz on-wake *  200 MHz up step (unchanged) *  400 MHz down step
    Change-Id: I60e9eaff0bbe85349416c93a3bc7dff4058f1663

--- drivers/cpufreq/cpufreq_smartass2.c
+++ drivers/cpufreq/cpufreq_smartass2.c
@@ -43,7 +43,7 @@
  * towards the ideal frequency and slower after it has passed it. Similarly,
  * lowering the frequency towards the ideal frequency is faster than below it.
  */
-#define DEFAULT_AWAKE_IDEAL_FREQ (800*1000)
+#define DEFAULT_AWAKE_IDEAL_FREQ (600*1000)
 static unsigned int awake_ideal_freq;
 
 /*
@@ -68,7 +68,7 @@ static unsigned int ramp_up_step;
  * Zero disables and will calculate ramp down according to load heuristic.
  * When above the ideal freqeuncy we always ramp down to the ideal freq.
  */
-#define DEFAULT_RAMP_DOWN_STEP (300*1000)
+#define DEFAULT_RAMP_DOWN_STEP (400*1000)
 static unsigned int ramp_down_step;
 
 /*
@@ -101,7 +101,7 @@ static unsigned long down_rate_us;
  * The frequency to set when waking up from sleep.
  * When sleep_ideal_freq=0 this will have no effect.
  */
-#define DEFAULT_SLEEP_WAKEUP_FREQ (800*1000)
+#define DEFAULT_SLEEP_WAKEUP_FREQ (1000*1000)
 static unsigned int sleep_wakeup_freq;
 
 /*
